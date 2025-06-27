37c53b015c92a5a48aad7ba6fee292dc1b8ab2b8
NelsonOrtiz94 <ortnn990@gmail.com>
2025-06-27 10:04:22 -0500

    Se definen y estructuran implementaciones

---
+++ src/main/java/services/impl/ServicioBImpl.java
@@ -0,0 +1,30 @@
+package services.impl;
+
+import services.ServicioA;
+import services.ServicioB;
+
+public class ServicioBImpl implements ServicioB {
+
+    // Atributo que almacena una referencia al servicio A (Inyección de dependencias)
+    private ServicioA servicioA;
+
+    @Override
+    public ServicioA getServicioA() {
+        return servicioA;
+    }
+
+    @Override
+    public void setServicioA(ServicioA servicioA) {
+        this.servicioA = servicioA;
+    }
+
+    @Override
+    public int multiplicarSumar(int a, int b, int multiplicador) {
+        return (servicioA.sumar(a, b)) * multiplicador;
+    }
+
+    @Override
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
